177ceac8de93679ad925dd5e5e3502c82bee98bf
MichaelCurrin <michael.currin@gmail.com>
2019-02-23 21:11:51 +0200

    feature: Add basic fetching of planet

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 const axios = require('axios');
-const baseUrl = 'https://swapi.co/api';
+const BASE_URL = "https://swapi.co/api";
 
 const { graphql, GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLNonNull } = require('graphql');
+
+
+/**
+ * Request a Star Wars object from the API and return it.
+ *
+ * @param string type Name of object type to lookup. Must be one of the values
+ *      defined here: https://swapi.co/documentation#root .
+ * @param numeric id The ID of the object to look up within the type.
+ */
+function request(type, id) {
+    let url = `${BASE_URL}/${type}/${id}/`;
+    console.log(url)
+    return axios.get(url)
+        .then(resp => resp.data);
+}
 
 
 const Planet = new GraphQLObjectType({
@@ -34,11 +49,7 @@ const Schema = new GraphQLSchema({
 
 const Resolvers = {
     planet() {
-        return {
-            climate: 'climate',
-            name: 'name',
-            terrain: 'terrain',
-        };
+        return request('planets', 1);
     }
 };
 
